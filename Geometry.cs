33530dcb4892a821ef745a0a0bdf1f804e2c25a2
Богдан <const776@mail.ru>
2022-11-25 15:38:17 +1000

    Добавьте файлы проекта.

---
+++ Geometry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIS_Shovkun_PR2
+{
+    public class Geometry
+    {
+        public static void Main(int[] args)
+        {
+
+        }
+
+        public static int RectangleArea(int a, int b)
+        {
+            int c = a * b;
+            return c;
+        }
+    }
+}
